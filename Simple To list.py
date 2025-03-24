439653e09296e7c1ecb4ca240b202249ee532d4c
Lesze <lesterandrew.arollado.cvt@eac.edu.ph>
2025-03-24 15:13:36 +0800

    Added another task

--- Simple To list.py
+++ Simple To list.py
@@ -1,12 +1,14 @@
 print("Welcome to do list organizer please Enter Your Task")
-# this is 3 Task organizer
+# this is 4 Task organizer
 First_User_task_Input = input("What is your First task?: " )
 Second_User_task_Input = input("What is your Second task?: " )
 Third_User_task_Input = input("What is your Third task?: " )
+fourth_User_task_Input = input("What is your Fourth task?: " )
 print("Please Enter the time you want to complete all of your tasks")
 Time_User_Task = input("What time do you want to complete your tasks?: " )
 print("Your First Task is: " + First_User_task_Input + " & You want to complete your task at " + Time_User_Task)
 print("Your Second Task is: " + Second_User_task_Input + " & You want to complete your task at " + Time_User_Task)
 print("Your Third Task is: " + Third_User_task_Input + " & You want to complete your task at " + Time_User_Task)
+print("Your fourth Task is: " + fourth_User_task_Input + " & You want to complete your task at " + Time_User_Task)
 # This shit needs continue and no and real time date on task time :>
 # But Good effort Over All so far Yay
